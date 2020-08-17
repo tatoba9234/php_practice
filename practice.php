2b469ef4211dc0260fc4c7f4fdce27f31bbc36e4
”Koyo” <tatoba9234@gmail.com>
2020-08-17 01:11:43 +0000

    assignment03

--- practice.php
+++ practice.php
@@ -1,2 +1,25 @@
 <?php
-echo 'hello php!';
+
+
+$country = "France";
+switch ($country) {
+    case "US":
+    case "Canada":
+     echo "North America";
+    break;
+    case "China":
+    case "Japan":
+    case "Korea":
+     echo "Asia";
+     break;
+    default: 
+     echo "Others";
+}
+
+$fruits = array ("apple", "banana", "lemon");
+echo count($fruits);
+
+for ($i = 0; $i < count($fruits); $i++) {
+    echo $fruits[$i];
+    echo "\n";
+}
